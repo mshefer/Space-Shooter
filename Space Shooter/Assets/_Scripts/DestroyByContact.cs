ef629cae283c6568d3fa87c62034b416818706a2
mshefer <42977579+mshefer@users.noreply.github.com>
2018-12-01 00:54:41 -0500

    5
    Final Instance

--- Space Shooter/Assets/_Scripts/DestroyByContact.cs
+++ Space Shooter/Assets/_Scripts/DestroyByContact.cs
@@ -7,6 +7,7 @@ public class DestroyByContact : MonoBehaviour
     public GameObject explosion;
     public GameObject playerExplosion;
     public int scoreValue;
+
     private GameController gameController;
 
     void Start()
@@ -24,12 +25,17 @@ public class DestroyByContact : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Boundary")
+        if (other.CompareTag("Boundary") || other.CompareTag("Enemy"))
         {
             return;
         }
-        Instantiate(explosion, transform.position, transform.rotation);
-        if (other.tag == "Player")
+
+        if (explosion != null)
+        {
+            Instantiate(explosion, transform.position, transform.rotation);
+        }
+
+        if (other.CompareTag("Player"))
         {
             Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
             gameController.GameOver();
